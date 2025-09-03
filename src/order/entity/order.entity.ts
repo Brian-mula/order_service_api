94f7f3023cb580eb43c67d2c9ac10c2b78e73230
Brian-mula <mulatiwangilabrian@gmail.com>
2025-09-03 23:01:57 +0300

    Chore:Add/ setup order entity and module

---
+++ src/order/entity/order.entity.ts
@@ -0,0 +1,30 @@
+import { User } from "src/user/entity/user.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, VersionColumn } from "typeorm";
+
+export enum OrderStatus {
+    PENDING = 'PENDING',
+    PAID = 'PAID',
+    CANCELLED = 'CANCELLED',
+}
+
+
+@Entity()
+export class Order {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @ManyToOne(()=> User, (user)=>user.id)
+    user:User;
+
+    @Column({type:'jsonb'})
+    items:{sku:string, quantity:number}[];
+
+    @Column({type:'enum', enum: OrderStatus, default: OrderStatus.PENDING})
+    status: OrderStatus;
+
+    @Column({unique:true})
+    clientToken: string;
+
+    @VersionColumn()
+    version: number;
+}
